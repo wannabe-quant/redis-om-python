ee3b07a628a15190456ddda4d87529429e4363bb
Wannabe Quant <mj563436@gmail.com>
2024-04-18 17:18:06 +0000

    updated .gitignore to include sync generated code, tests in the package

---
+++ tests_sync/test_redis_type.py
@@ -0,0 +1,10 @@
+from redis_om import redis
+from redis_om.util import ASYNC_MODE
+
+
+def test_redis_type():
+    import redis as sync_redis_module
+    import redis.asyncio as async_redis_module
+
+    mapping = {True: async_redis_module, False: sync_redis_module}
+    assert mapping[ASYNC_MODE] is redis
